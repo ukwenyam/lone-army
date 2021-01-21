a123b3f98a2e5d03adaae3763c2595467f13ba6b
ukwenyam <commyjunior@gmail.com>
2021-01-20 21:42:18 -0600

    init commit

---
+++ app/src/main/java/com/example/lonearmy/LoneArmyActivity.java
@@ -0,0 +1,14 @@
+package com.example.lonearmy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoneArmyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
